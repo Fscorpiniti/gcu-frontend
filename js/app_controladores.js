6a279d1ca9ace0adc0ac8a9bc5cdb70a9ce2bc5e
Guillermo Arispe <guillermo.arispe@gmail.com>
2014-11-01 17:31:40 -0300

    se commitea el comienzo de la planificacion semanal

--- js/app_controladores.js
+++ js/app_controladores.js
@@ -6,6 +6,8 @@ var App = angular.module('Listado_Materias', []);
         var url_carreras  = 'http://gcu-api.herokuapp.com:80/carreras/1/planes';
         var url_planMaterias = 'http://gcu-api.herokuapp.com:80/planes/1/materias';
         var url_posiblesCursantes = 'http://gcu-api.herokuapp.com:80/planes/1/materias/probables-cursantes'
+        var url_planificacionPares = 'http://gcu-api.herokuapp.com:80/par/planificacion'
+        var url_planificacionImpares = 'http://gcu-api.herokuapp.com:80/impar/planificacion'
         
          $http({method: 'GET', 
                url: url_carreras
@@ -26,5 +28,20 @@ var App = angular.module('Listado_Materias', []);
            
             $scope.posiblesCursantes = data; 
         });
+        
+        $http({method: 'GET', 
+               url: url_planificacionPares
+         }).success(function(data){
+           
+            $scope.planificacionSemana = data; 
+        });
+        
+        $http({method: 'GET', 
+               url: url_planificacionImpares
+         }).success(function(data){
+           
+            $scope.planificacionSemana = data; 
+        });
+        
 
     });
