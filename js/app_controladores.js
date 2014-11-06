6dba120f642d6ceed0df77ad895e8ba8ae432873
fernando <fernando.scorpiniti@sayqa.com>
2014-11-06 14:48:41 -0300

    Se hace un solo request para cargar todo.

--- js/app_controladores.js
+++ js/app_controladores.js
@@ -6,16 +6,8 @@ var App = angular.module('Listado_Materias', []);
         var url_carreras  = 'http://gcu-api.herokuapp.com:80/carreras/1/planes';
         var url_planMaterias = 'http://gcu-api.herokuapp.com:80/planes/1/materias';
         var url_posiblesCursantes = 'http://gcu-api.herokuapp.com:80/planes/1/materias/probables-cursantes'
-        var url_planificacion_primer_cuatri = 'http://localhost:8080/gcu/planificacion/1/1'
-        var url_planificacion_segundo_cuatri = 'http://localhost:8080/gcu/planificacion/1/2'
-        var url_planificacion_tercer_cuatri = 'http://localhost:8080/gcu/planificacion/2/1'
-        var url_planificacion_cuarto_cuatri = 'http://localhost:8080/gcu/planificacion/2/2'
-        var url_planificacion_quinto_cuatri = 'http://localhost:8080/gcu/planificacion/3/1'
-        var url_planificacion_sexto_cuatri = 'http://localhost:8080/gcu/planificacion/3/2'
-        var url_planificacion_septimo_cuatri = 'http://localhost:8080/gcu/planificacion/4/1'
-        var url_planificacion_octavo_cuatri = 'http://localhost:8080/gcu/planificacion/4/2'
-        var url_planificacion_noveno_cuatri = 'http://localhost:8080/gcu/planificacion/5/1'
-        var url_planificacion_decimo_cuatri = 'http://localhost:8080/gcu/planificacion/5/2'
+        var url_planificacion_cuatri_par = 'http://localhost:8080/gcu/planificacion/par'
+        var url_planificacion_cuatri_impar = 'http://localhost:8080/gcu/planificacion/impar'
         
          $http({method: 'GET', 
                url: url_carreras
@@ -38,74 +30,25 @@ var App = angular.module('Listado_Materias', []);
         });
         
         $http({method: 'GET', 
-               url: url_planificacion_primer_cuatri
+               url: url_planificacion_cuatri_par
          }).success(function(data){
            
-            $scope.planificacionPrimerCuatri = data;
+            $scope.planificacionSegundoCuatri = data.nivelesSegundoCuatri;
+            $scope.planificacionCuartoCuatri = data.nivelesCuartoCuatri;
+            $scope.planificacionSextoCuatri = data.nivelesSextoCuatri;
+            $scope.planificacionOctavoCuatri = data.nivelesOctavoCuatri;
+            $scope.planificacionDecimoCuatri = data.nivelesDecimoCuatri;
         });
         
         $http({method: 'GET', 
-               url: url_planificacion_segundo_cuatri
+               url: url_planificacion_cuatri_impar
          }).success(function(data){
            
-            $scope.planificacionSegundoCuatri = data;
+            $scope.planificacionPrimerCuatri = data.nivelesPrimerCuatri;
+            $scope.planificacionTercerCuatri = data.nivelesTercerCuatri;
+            $scope.planificacionQuintoCuatri = data.nivelesQuintoCuatri;
+            $scope.planificacionSeptimoCuatri = data.nivelesSeptimoCuatri;
+            $scope.planificacionNovenoCuatri = data.nivelesNovenoCuatri;
         });
-
-        $http({method: 'GET',
-            url: url_planificacion_tercer_cuatri
-        }).success(function(data){
-
-            $scope.planificacionTercerCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_cuarto_cuatri
-        }).success(function(data){
-
-            $scope.planificacionCuartoCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_quinto_cuatri
-        }).success(function(data){
-
-            $scope.planificacionQuintoCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_sexto_cuatri
-        }).success(function(data){
-
-            $scope.planificacionSextoCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_septimo_cuatri
-        }).success(function(data){
-
-            $scope.planificacionSeptimoCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_octavo_cuatri
-        }).success(function(data){
-
-            $scope.planificacionOctavoCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_noveno_cuatri
-        }).success(function(data){
-
-            $scope.planificacionNovenoCuatri = data;
-        });
-
-        $http({method: 'GET',
-            url: url_planificacion_decimo_cuatri
-        }).success(function(data){
-
-            $scope.planificacionDecimoCuatri = data;
-        });
-        
 
     });
