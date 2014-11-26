dd32a5c1ab40cd3ae39cdbefbf1a3b9dfcbef406
fernando <fernando.scorpiniti@gmail.com>
2014-11-25 23:45:04 -0300

    Se desglosa el score.

--- js/app_controladores.js
+++ js/app_controladores.js
@@ -2,19 +2,19 @@
 var App = angular.module('Listado_Materias', []);
 
     App.controller('Controlador_GET', function($scope, $http){
+
+        var url_carreras  = 'http://localhost:8080/gcu/carreras/1/planes';
+        var url_planMaterias = 'http://localhost:8080/gcu/planes/1/materias';
+        var url_posiblesCursantes = 'http://localhost:8080/gcu/planes/1/materias/probables-cursantes'
+        var url_planificacion_cuatri_par = 'http://localhost:8080/gcu/planificacion/par'
+        var url_planificacion_cuatri_impar = 'http://localhost:8080/gcu/planificacion/impar'
         
-        var url_carreras  = 'http://gcu-api.herokuapp.com:80/carreras/1/planes';
-        var url_planMaterias = 'http://gcu-api.herokuapp.com:80/planes/1/materias';
-        var url_posiblesCursantes = 'http://gcu-api.herokuapp.com:80/planes/1/materias/probables-cursantes'
-        var url_planificacion_cuatri_par = 'http://gcu-api.herokuapp.com:80/planificacion/par'
-        var url_planificacion_cuatri_impar = 'http://gcu-api.herokuapp.com:80/planificacion/impar'
-        
-         $http({method: 'GET', 
+        $http({method: 'GET',
                url: url_carreras
           }).success(function(data){
              $scope.planes = data;
         })
-        
+
         $http({method: 'GET', 
                url: url_planMaterias
          }).success(function(data){
@@ -32,7 +32,13 @@ var App = angular.module('Listado_Materias', []);
         $http({method: 'GET', 
                url: url_planificacion_cuatri_par
          }).success(function(data){
-            $scope.scorePar = data.scores;
+            $scope.scoresPares = data.scores;
+            $scope.scoreUnificadoPar = data.score;
+            $scope.scoreSegundoCuatri = data.scoreSegundoCuatri;
+            $scope.scoreCuartoCuatri = data.scoreCuartoCuatri;
+            $scope.scoreSextoCuatri = data.scoreSextoCuatri;
+            $scope.scoreOctavoCuatri = data.scoreOctavoCuatri;
+            $scope.scoreDecimoCuatri = data.scoreDecimoCuatri;
             $scope.planificacionSegundoCuatri = data.nivelesSegundoCuatri;
             $scope.planificacionCuartoCuatri = data.nivelesCuartoCuatri;
             $scope.planificacionSextoCuatri = data.nivelesSextoCuatri;
@@ -43,7 +49,13 @@ var App = angular.module('Listado_Materias', []);
         $http({method: 'GET', 
                url: url_planificacion_cuatri_impar
          }).success(function(data){
-            $scope.scoreImpar = data.scores;
+            $scope.scoresImpares = data.scores;
+            $scope.scoreUnificadoImpar = data.score;
+            $scope.scorePrimerCuatri = data.scorePrimerCuatri;
+            $scope.scoreTercerCuatri = data.scoreTercerCuatri;
+            $scope.scoreQuintoCuatri = data.scoreQuintoCuatri;
+            $scope.scoreSeptimoCuatri = data.scoreSeptimoCuatri;
+            $scope.scoreNovenoCuatri = data.scoreNovenoCuatri;
             $scope.planificacionPrimerCuatri = data.nivelesPrimerCuatri;
             $scope.planificacionTercerCuatri = data.nivelesTercerCuatri;
             $scope.planificacionQuintoCuatri = data.nivelesQuintoCuatri;
