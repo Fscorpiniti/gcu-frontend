5c4ff8ccb694502ec36d71e92f1201e52a2be0fc
Guillermo Arispe <guillermo.arispe@gmail.com>
2014-12-01 09:48:35 -0300

    Se agrega pantalla de subir archivo

--- js/subir_archivo.js
+++ js/subir_archivo.js
@@ -1,12 +1,17 @@
 App.directive('subirArchivo', function () {
+    
         return {
-            scope: true,        //create a new scope
+            
+            scope: true,        
+            
             link: function (scope, el, attrs) {
+                
                 el.bind('change', function (event) {
+                    
                     var files = event.target.files;
-                    //iterate files since 'multiple' may be specified on the element
-                    for (var i = 0;i<files.length;i++) {
-                        //emit event upward
+                   
+                    for (var i = 0; i < files.length; i++) {
+                    
                         scope.$emit("fileSelected", { file: files[i] });
                     }                                       
                 });
